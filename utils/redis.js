cf790613491cd43c484c4d9b0b98d37e3d692828
kimenyu <njorogekimenyu@gmail.com>
2023-12-04 12:36:58 +0300

    redis.js

---
+++ utils/redis.js
@@ -0,0 +1,41 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+
+        // Handling errors
+        this.client.on('error', (err) => {
+            console.error(`Redis client error: ${err}`);
+        });
+    }
+
+    isAlive() {
+        // Checking the connection status
+        return this.client.connected;
+    }
+
+    async get(key) {
+        // Using promisify for the get method
+        const getAsync = promisify(this.client.get).bind(this.client);
+        return await getAsync(key);
+    }
+
+    async set(key, value, duration) {
+        // Using promisify for the set method
+        const setAsync = promisify(this.client.set).bind(this.client);
+        return await setAsync(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        // Using promisify for the del method
+        const delAsync = promisify(this.client.del).bind(this.client);
+        return await delAsync(key);
+    }
+}
+
+// Exporting an instance of RedisClient
+const redisClient = new RedisClient();
+
+export default redisClient;
